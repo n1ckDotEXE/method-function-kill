6b66205cc9d68d23cadd1b9e75fd9682d18402a5
n1ckDotEXE <admin@nicholasnam.com>
2020-12-14 10:24:07 -0500

    complete

--- main.js
+++ main.js
@@ -1,8 +1,48 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+const Person = function(newFirstName, newLastName, newAge = 0, newMarried = false) {
+	return {
+		firstName : newFirstName,
+		lastName : newLastName, 
+		age : newAge, 
+		married : newMarried,
 
+		getFirstName: function() {
+		return `${this.firstName}`;
+		},
 
+		getLastName: function() {
+		return `${this.lastName}`;
+		},
+
+		goingOn: function() {
+		return this.age + 1;
+		},
+
+		ageUp: function() {
+		return this.age++;
+		},
+
+		getFullName: function() {
+		return `${this.firstName} ${this.lastName}`;
+		},
+
+		marry: function(person) {
+		this.married = true;
+		person.married = true;
+		this.spouseName = person.getFullName();
+		person.spouseName = this.getFullName();
+		},
+
+		divorce: function(person) {
+		this.married = false;
+		person.married = false;
+		delete this.spouseName;
+		delete person.spouseName;
+		}
+	}
+}
 
 /*********************************
  * OUR CODE BELOW; DO NOT TOUCH! *
@@ -11,6 +51,5 @@
 if (typeof Person === 'undefined') {
   Person = undefined;
 }
-
 
 module.exports = Person;
